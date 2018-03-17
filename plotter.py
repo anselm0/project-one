805d39bb90878277fc103a0a17304efae5f126e1
Anselmo Jr Garza <anselmojrgarza@LeMac.attlocal.net>
2018-03-16 22:41:46 -0500

    adding plots, updating plotter.py

--- plotter.py
+++ plotter.py
@@ -4,9 +4,7 @@ import datetime
 
 target_path = "Merged/"
 
-target_csv_list = [target_path + "@WSJ_all.csv"]
-                   
-'''
+target_csv_list = [target_path + "@WSJ_all.csv",
                    target_path + "@FT_all.csv", 
                    target_path + "@nyTimes_all.csv", 
                    target_path + "@barronsonline_all.csv", 
@@ -16,14 +14,22 @@ target_csv_list = [target_path + "@WSJ_all.csv"]
                    target_path + "@business_all.csv", 
                    target_path + "@USAToday_all.csv", 
                    target_path + "@FoxBusiness_all.csv"]
-                                                        '''
+                                                        
 for csv_item in target_csv_list:
+    
+    font = {'family': 'sans-serif',
+            'color':  'darkred',
+            'weight': 'bold',
+            'size': 18,
+            }
     
     df = pd.read_csv(csv_item, index_col = False)
     df['Date'] = pd.to_datetime(df['Date'], format= '%Y-%m-%d')
     x = df['Date'].values
     vader = df['Vader'].values
     textblob = df['TextBlob'].values
+    plot_path = "Plots/"
+
     
     #SPX
     y = df['SPX Price'].values
@@ -35,15 +41,19 @@ for csv_item in target_csv_list:
     ax.set_ylim(2600, 2800)
     ax.set_xlim([datetime.date(2018,2,10), datetime.date(2018,3,18)])
     
-    ax.scatter(x, y, s=volume, c=vader, cmap='RdYlGn', alpha=0.5)
+    sc = ax.scatter(x, y, s=volume, c=vader, cmap='RdYlGn', alpha=0.5, edgecolor = 'black')
     ax.plot(x,y)
     
-    ax.grid(True)
+    ax.grid(True, alpha=.5)
     fig.tight_layout()
     
-    
-    
-    ax.set_title('WS_SPX')
+    title = '{} Sentiment vs SP500'.format(csv_item[7:-8])
+    ax.set_title(str(title), fontdict=font)
+    ax.set_ylabel('Price')
+    plt.xticks(rotation=45)
+    fig.colorbar(sc, spacing='proportional').set_label('Sentiment Polarity',rotation = 270,labelpad=10)
+    fig.set_size_inches(9, 6)
+    fig.savefig(plot_path + csv_item[7:-8] + '_SPX_plot.png',bbox_inches="tight", dpi=72)  
     
     #DJI
     y = df['DJI Price'].values
@@ -56,15 +66,20 @@ for csv_item in target_csv_list:
     ax.set_xlim([datetime.date(2018,2,10), datetime.date(2018,3,18)])
 
     
-    ax.scatter(x, y, s=volume_dji, c=vader, cmap='RdYlGn', alpha=0.5)
+    sc = ax.scatter(x, y, s=volume_dji, c=vader, cmap='RdYlGn', alpha=0.5, edgecolor = 'black')
     ax.plot(x,y)
     
-    ax.grid(True)
+    ax.grid(True, alpha=.5)
     fig.tight_layout()
     
-    
-    
-    ax.set_title('WSJ_DJI')
+    title = '{} Sentiment vs Dow Jones'.format(csv_item[7:-8])
+    ax.set_title(str(title), fontdict=font)
+    ax.set_ylabel('Price')
+    plt.xticks(rotation=45)
+    fig.colorbar(sc, spacing='proportional').set_label('Sentiment Polarity',rotation = 270,labelpad=10)
+    fig.set_size_inches(9, 6)
+    fig.savefig(plot_path + csv_item[7:-8] + '_DJI_plot.png',bbox_inches="tight", dpi=72)  
+
     
     #NDX
     y = df['NDX Price'].values
@@ -76,21 +91,23 @@ for csv_item in target_csv_list:
     ax.set_ylim(6400, 7200)
     ax.set_xlim([datetime.date(2018,2,10), datetime.date(2018,3,18)])
     
-    ax.scatter(x, y, s=volume_ndx, c=vader, cmap='RdYlGn', alpha=0.5)
+    sc = ax.scatter(x, y, s=volume_ndx, c=vader, cmap='RdYlGn', alpha=0.5, edgecolor = 'black')
     ax.plot(x,y)
 
     
-    ax.grid(True)
+    ax.grid(True, alpha=.5)
     fig.tight_layout()
     
     
-    
-    ax.set_title('WSJ_NDX')
-    
+    title = '{} Sentiment vs NASDAQ'.format(csv_item[7:-8])
+    ax.set_title(str(title), fontdict=font)
+    ax.set_ylabel('Price')
+    plt.xticks(rotation=45)
+    fig.colorbar(sc, spacing='proportional').set_label('Sentiment Polarity',rotation = 270,labelpad=10)
+    fig.set_size_inches(9, 6)
+    fig.savefig(plot_path + csv_item[7:-8] + '_NDX_plot.png',bbox_inches="tight", dpi=72)  
+
     #add legends and fix x labels
     #make graph larger
-    #border around bubbles
-    #add interactivity with mouse overs
-    #save the figures as jpgs to a folder
-    
+    #add interactivity with mouse overs    
     
